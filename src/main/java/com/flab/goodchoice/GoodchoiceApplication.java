79d871d61a7a90c09ec163984ad99493b923995c
KATEKEITH <46472768+KATEKEITH@users.noreply.github.com>
2023-05-05 13:27:49 +0900

    init project

---
+++ src/main/java/com/flab/goodchoice/GoodchoiceApplication.java
@@ -0,0 +1,13 @@
+package com.flab.goodchoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodchoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodchoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/goodchoice/GoodchoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.goodchoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodchoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
