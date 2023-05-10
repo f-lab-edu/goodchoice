623ac377ac5b83ed25841315f9a85faa354640b2
choiseon <com1368@naver.com>
2023-05-11 01:06:13 +0900

    fix : 클래스명 구체적 역할이 알 수 있도록 변경

--- src/main/java/com/flab/goodchoice/coupon/api/CouponController.java
+++ src/main/java/com/flab/goodchoice/coupon/api/CouponController.java
@@ -1,6 +1,7 @@
 package com.flab.goodchoice.coupon.api;
 
-import com.flab.goodchoice.coupon.application.CouponService;
+import com.flab.goodchoice.coupon.application.CouponCommandService;
+import com.flab.goodchoice.coupon.application.CouponQueryService;
 import com.flab.goodchoice.coupon.dto.CouponInfoResponse;
 import com.flab.goodchoice.coupon.dto.CreateCouponRequest;
 import com.flab.goodchoice.coupon.dto.ModifyCouponRequest;
@@ -14,34 +15,41 @@ import java.util.UUID;
 @RestController
 public class CouponController {
 
-    private final CouponService couponService;
+    private final CouponQueryService couponQueryService;
+    private final CouponCommandService couponCommandService;
 
-    public CouponController(final CouponService couponService) {
-        this.couponService = couponService;
+    public CouponController(CouponQueryService couponQueryService, CouponCommandService couponCommandService) {
+        this.couponQueryService = couponQueryService;
+        this.couponCommandService = couponCommandService;
     }
 
     @PostMapping
     public UUID create(@RequestBody final CreateCouponRequest createCouponRequest) {
         validation(createCouponRequest.couponName(), createCouponRequest.stock());
 
-        return couponService.create(createCouponRequest.couponName(), createCouponRequest.stock());
+        return couponCommandService.create(createCouponRequest.couponName(), createCouponRequest.stock());
     }
 
     @GetMapping
     public List<CouponInfoResponse> getAllCoupons() {
-        return couponService.getAllCoupons();
+        return couponQueryService.getAllCoupons();
     }
 
     @GetMapping("/{couponToken}")
     public CouponInfoResponse getCouponDetail(@PathVariable final UUID couponToken) {
-        return couponService.getCouponDetail(couponToken);
+        return couponQueryService.getCouponDetail(couponToken);
     }
 
     @PutMapping("/{couponToken}")
     public UUID modifyCoupon(@PathVariable final UUID couponToken, @RequestBody final ModifyCouponRequest modifyCouponRequest) {
         validation(modifyCouponRequest.couponName(), modifyCouponRequest.stock());
 
-        return couponService.modifyCoupon(couponToken, modifyCouponRequest.couponName(), modifyCouponRequest.stock());
+        return couponCommandService.modifyCoupon(couponToken, modifyCouponRequest.couponName(), modifyCouponRequest.stock());
+    }
+
+    @DeleteMapping("/{couponToken}")
+    public UUID deleteCoupon(@PathVariable final UUID couponToken) {
+        return couponCommandService.deleteCoupon(couponToken);
     }
 
     private void validation(final String couponName, final int stock) {
@@ -52,10 +60,5 @@ public class CouponController {
         if (stock < 0) {
             throw new IllegalArgumentException("쿠폰 갯수는 음수가 될수 없습니다.");
         }
-    }
-
-    @DeleteMapping("/{couponToken}")
-    public UUID deleteCoupon(@PathVariable final UUID couponToken) {
-        return couponService.deleteCoupon(couponToken);
     }
 }
--- src/main/java/com/flab/goodchoice/coupon/application/CouponCommandService.java
+++ src/main/java/com/flab/goodchoice/coupon/application/CouponCommandService.java
@@ -3,24 +3,21 @@ package com.flab.goodchoice.coupon.application;
 import com.flab.goodchoice.coupon.domain.Coupon;
 import com.flab.goodchoice.coupon.domain.State;
 import com.flab.goodchoice.coupon.domain.repositories.CouponRepository;
-import com.flab.goodchoice.coupon.dto.CouponInfoResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.UUID;
 
-@Transactional(readOnly = true)
+@Transactional
 @Service
-public class CouponService {
+public class CouponCommandService {
 
     private final CouponRepository couponRepository;
 
-    public CouponService(CouponRepository couponRepository) {
+    public CouponCommandService(CouponRepository couponRepository) {
         this.couponRepository = couponRepository;
     }
 
-    @Transactional
     public UUID create(final String couponName, final int stock) {
         Coupon coupon = new Coupon(UUID.randomUUID(), couponName, stock, State.ACTIVITY);
         couponRepository.save(coupon);
@@ -28,18 +25,6 @@ public class CouponService {
         return coupon.getCouponToken();
     }
 
-    public List<CouponInfoResponse> getAllCoupons() {
-        return couponRepository.findAll().stream()
-                .map(coupon -> new CouponInfoResponse(coupon.getCouponToken(), coupon.getCouponName(), coupon.getStock(), coupon.getState()))
-                .toList();
-    }
-
-    public CouponInfoResponse getCouponDetail(final UUID couponToken) {
-        Coupon coupon = couponRepository.findByCouponToken(couponToken).orElseThrow(() -> new IllegalArgumentException("해당 쿠폰을 찾을 수 없습니다."));
-        return new CouponInfoResponse(coupon.getCouponToken(), coupon.getCouponName(), coupon.getStock(), coupon.getState());
-    }
-
-    @Transactional
     public UUID modifyCoupon(final UUID couponToken, final String couponName, final int stock) {
         Coupon coupon = couponRepository.findByCouponToken(couponToken).orElseThrow(() -> new IllegalArgumentException("해당 쿠폰을 찾을 수 없습니다."));
         coupon.modify(couponName, stock);
@@ -47,7 +32,6 @@ public class CouponService {
         return coupon.getCouponToken();
     }
 
-    @Transactional
     public UUID deleteCoupon(UUID couponToken) {
         Coupon coupon = couponRepository.findByCouponToken(couponToken).orElseThrow(() -> new IllegalArgumentException("해당 쿠폰을 찾을 수 없습니다."));
         coupon.delete();
