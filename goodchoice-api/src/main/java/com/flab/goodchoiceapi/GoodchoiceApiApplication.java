9c7290d95ec64e4e4ea772d61deb86b1a3e6fdb3
choiseonkang <com1368@naver.com>
2023-07-16 23:44:36 +0900

    [#40] feat : 멀티 모듈 설정

---
+++ goodchoice-api/src/main/java/com/flab/goodchoiceapi/GoodchoiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.flab.goodchoiceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodchoiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodchoiceApiApplication.class, args);
+    }
+
+}
