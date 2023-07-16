61e8d7af648fe905030d1622032742f9062460fb
choiseonkang <com1368@naver.com>
2023-07-16 23:46:28 +0900

    [#40] feat : 멀티 모듈 설정

---
+++ goodchoice-batch/src/main/java/com/flab/goodchoicebatch/GoodchoiceBatchApplication.java
@@ -0,0 +1,13 @@
+package com.flab.goodchoicebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodchoiceBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodchoiceBatchApplication.class, args);
+    }
+
+}
