403e28d064e99093024ad6e7d120a88137d4ac40
KATEKEITH <46472768+KATEKEITH@users.noreply.github.com>
2023-07-29 13:58:28 +0900

    #50 fix 유효성 체크 로직 item domain 내부로 이동

--- goodchoice-item/src/main/java/com/flab/goodchoiceitem/infrastructure/ItemStoreImpl.java
+++ goodchoice-item/src/main/java/com/flab/goodchoiceitem/infrastructure/ItemStoreImpl.java
@@ -5,10 +5,7 @@ import com.flab.goodchoiceitem.domain.ItemStore;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
-
-import java.security.InvalidParameterException;
 
 @Slf4j
 @Component
@@ -19,21 +16,7 @@ public class ItemStoreImpl implements ItemStore {
 
     @Override
     public Item store(Item item) {
-        validCheck(item);
         return itemRepository.saveAndFlush(item);
-    }
-
-    private void validCheck(Item item) {
-        if (StringUtils.isEmpty(item.getItemToken()))
-            throw new InvalidParameterException("Item.itemToken");
-        if (StringUtils.isEmpty(item.getItemName()))
-            throw new InvalidParameterException("Item.itemName");
-        if (item.getItemPrice() == null)
-            throw new InvalidParameterException("Item.itemPrice");
-        if (item.getItemStock() == null)
-            throw new InvalidParameterException("Item.itemStock");
-        if (item.getStatus() == null)
-            throw new InvalidParameterException("Item.status");
     }
 
 }
